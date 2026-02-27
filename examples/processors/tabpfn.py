2f1dded921c0422ed7be87565e8bd226ea0c48a5
maxbrzr <maximilian.burzer@kit.edu>
2026-02-27 17:49:37 +0100

    update tabpfn classifier

--- examples/processors/tabpfn.py
+++ examples/processors/tabpfn.py
@@ -6,15 +6,17 @@ from dcat_ap_hub.core.interfaces import SKLearnModel
 from dcat_ap_hub.internals.utils import from_import_or_install
 
 
-class TabPFNModel(SKLearnModel):
+class TabPFNClassifier(SKLearnModel):
     def __init__(self) -> None:
         super().__init__()
 
-        TabPFNRegressor = from_import_or_install("tabpfn", "tabpfn", "TabPFNRegressor")
+        TabPFNClassifier = from_import_or_install(
+            "tabpfn", "tabpfn", "TabPFNClassifier"
+        )
         ModelVersion = from_import_or_install(
             "tabpfn", "tabpfn.constants", "ModelVersion"
         )
-        self.clf = TabPFNRegressor.create_default_for_version(ModelVersion.V2)
+        self.clf = TabPFNClassifier.create_default_for_version(ModelVersion.V2)
 
     def fit(self, X_train: ndarray, y_train: ndarray) -> None:
         return self.clf.fit(X_train, y_train)
